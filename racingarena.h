6a14399c6596b31020e7a9682c04be088e81e19e
Marshall Ruse <12mr9@queensu.ca>
2016-12-12 16:01:19 -0500

    Create racingarena.h

---
+++ racingarena.h
@@ -0,0 +1,13 @@
+#ifndef RACINGARENA_H_
+#define RACINGARENA_H_
+
+int car2;
+int car3;
+int car4;
+int car5;
+
+int rc;
+double randNum;
+int winState = 1; //decrement to 0 on winning
+
+#endif /* RACINGARENA_H_ */
